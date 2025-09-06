8efa73b314478d29d44f531b05195e114d7a6d09
mikhail-ten <mishaten9957@gmail.com>
2025-09-06 15:37:56 +0200

    refactor: generate header links from data

---
+++ tests/header-navigation.test.ts
@@ -0,0 +1,10 @@
+/**
+ * @manual
+ *
+ * Vérifie que le menu de navigation affiche chaque chapitre et ses sous-chapitres.
+ * 1. Exécuter `npm run dev`.
+ * 2. Ouvrir la page d'accueil dans un navigateur.
+ * 3. Dans l'en-tête, s'assurer que les chapitres I à VI sont présents.
+ * 4. Pour chaque chapitre, cliquer et vérifier que "Sous-chapitre 1" et "Sous-chapitre 2" apparaissent avec les bons liens.
+ */
+export {};
